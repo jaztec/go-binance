8ebf06fc0582c5b87f67fd479fa5a873030ad4f7
Jasper van Herpt <jasper.v.herpt@gmail.com>
2021-04-30 01:54:58 +0200

    Encapsulate prices inside a collection

--- model/price.go
+++ model/price.go
@@ -7,10 +7,16 @@ type Price struct {
 	RawPrice string `json:"price"`
 }
 
-func (bp Price) Price() float64 {
-	f, err := strconv.ParseFloat(bp.RawPrice, 32)
+func (p Price) Price() float64 {
+	f, err := strconv.ParseFloat(p.RawPrice, 32)
 	if err != nil {
 		panic(err)
 	}
 	return f
 }
+
+type PriceCollection []Price
+
+func (pc PriceCollection) Len() int           { return len(pc) }
+func (pc PriceCollection) Swap(i, j int)      { pc[i], pc[j] = pc[j], pc[i] }
+func (pc PriceCollection) Less(i, j int) bool { return pc[i].Price() < pc[j].Price() }
