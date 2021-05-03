37b359f174bd4ddfe9c09aba0c5015ebd45171b3
Jasper van Herpt <jasper.v.herpt@gmail.com>
2021-05-03 18:09:46 +0200

    Increase the allowed running time a bit
    The CI server isn't that fast

--- parameters_test.go
+++ parameters_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Parameters", func() {
 			Expect(p.Encode()).To(Equal("mies=merel&aap=noot"))
 		})
 
-		Ω(runtime.Microseconds()).Should(BeNumerically("<", 5), "Encode() shouldn't take too long.")
+		Ω(runtime.Microseconds()).Should(BeNumerically("<", 20), "Encode() shouldn't take too long.")
 		b.RecordValue("microseconds run", float64(runtime.Microseconds()))
 	}, 10)
 })
