e001c25e0c29a22194f229570b3c2f9487737fc1
Jasper van Herpt <jasper.van.herpt@omines.com>
2021-05-09 01:51:18 +0200

    Change Makefile into script

---
+++ scripts/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+FILES=$(go list $PWD/... | grep -v vendor)
+
+mkdir -p artifacts
+go vet $FILES
+golint $FILES
+go test $FILES -bench=. -race -timeout 10000ms -coverprofile artifacts/cover.out
+go tool cover -html=artifacts/cover.out -o artifacts/cover.html
+go tool cover -func=artifacts/cover.out
