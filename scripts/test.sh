f1da58b2cb0e5d7ca339c7c3bf985396c35cc7c4
Jasper van Herpt <jasper.van.herpt@omines.com>
2021-05-09 22:41:08 +0200

    Add codecov line in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,3 +8,6 @@ golint $FILES
 go test $FILES -bench=. -race -timeout 10000ms -coverprofile artifacts/cover.out
 go tool cover -html=artifacts/cover.out -o artifacts/cover.html
 go tool cover -func=artifacts/cover.out
+
+# To get codecov to accept us
+go test -race -coverprofile=coverage.txt -covermode=atomic
